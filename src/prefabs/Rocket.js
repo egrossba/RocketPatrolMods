7daef20697fb609202aecdeecb4c548f5b31674f
Ethan Grossbach <egrossba@ucsc.edu>
2021-04-09 14:01:31 -0700

    yes

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -6,14 +6,21 @@ class Rocket extends Phaser.GameObjects.Sprite {
         // add object to the existing scene
         scene.add.existing(this);
         this.movementSpeed = 2;
+        this.isFiring = false;
     }
 
     update() {
+        if(this.isFiring){
+
+        }
+        
         if(keyLEFT.isDown){
             this.x -= this.movementSpeed;
         }
         if(keyRIGHT.isDown){
             this.x += this.movementSpeed;
         }
+
+        this.x = Phaser.Math.Clamp(this.x, borderUISize+borderPadding, game.config.width=borderUISize-borderPadding);
     }
 }
