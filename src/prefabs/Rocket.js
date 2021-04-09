a2e521a20d882f2bed4fd4e1d2c4ca6e4bccce62
Ethan Grossbach <egrossba@ucsc.edu>
2021-04-09 14:22:26 -0700

    destroy

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -1,26 +1,40 @@
-// Rocket (player) prefab
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
-
-        // add object to the existing scene
         scene.add.existing(this);
-        this.movementSpeed = 2;
+        this.movementSpeed = 4;
         this.isFiring = false;
     }
 
     update() {
-        if(this.isFiring){
+        if(this.isFiring) {
+            this.y -= this.movementSpeed;
+            if(this.y < borderUISize*3) {
+                this.y = game.config.height-borderUISize-borderPadding;
+                this.isFiring = false;
+            }
+        } else {
+            if(keyLEFT.isDown) {
+                this.x -= this.movementSpeed;
+            }
+            if(keyRIGHT.isDown) {
+                this.x += this.movementSpeed;
+            }
 
+            if(Phaser.Input.Keyboard.JustDown(keyF)) {
+                this.isFiring = true;
+            }
+    
+            this.x = Phaser.Math.Clamp(
+                this.x,
+                borderUISize + borderPadding,
+                game.config.width - borderUISize - borderPadding);
         }
         
-        if(keyLEFT.isDown){
-            this.x -= this.movementSpeed;
-        }
-        if(keyRIGHT.isDown){
-            this.x += this.movementSpeed;
-        }
+    }
 
-        this.x = Phaser.Math.Clamp(this.x, borderUISize+borderPadding, game.config.width=borderUISize-borderPadding);
+    reset() {
+        this.y = game.config.height-borderUISize-borderPadding;
+        this.isFiring = false;
     }
 }
