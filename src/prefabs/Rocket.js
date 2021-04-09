f66209a6494c728cc70307c6dc9817e21bebd482
Ethan Grossbach <egrossba@ucsc.edu>
2021-04-09 13:51:31 -0700

    ok

---
+++ src/prefabs/Rocket.js
@@ -0,0 +1,19 @@
+// Rocket (player) prefab
+class Rocket extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        // add object to the existing scene
+        scene.add.existing(this);
+        this.movementSpeed = 2;
+    }
+
+    update() {
+        if(keyLEFT.isDown){
+            this.x -= this.movementSpeed;
+        }
+        if(keyRIGHT.isDown){
+            this.x += this.movementSpeed;
+        }
+    }
+}
