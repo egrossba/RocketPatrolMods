9cf9812f7849a4bc856af9c59f88cb95d8b974f0
Ethan Grossbach <egrossba@ucsc.edu>
2021-04-20 18:02:36 -0700

    new menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,30 +8,40 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', 'assets/blip_select12.wav');
         this.load.audio('sfx_explosion', 'assets/explosion38.wav');
         this.load.audio('sfx_rocket', 'assets/rocket_shot.wav');
+
+        this.load.image('rocketPatrolMenu', 'assets/rocketPatrolMenu.png');
     }
 
     create() {
-        let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-        }
+        
+        this.rocketPatrolMenu = this.add.tileSprite(
+            0,30,0,0, 'rocketPatrolMenu'
+        ).setOrigin(0,0);
 
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize -
-            borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 
-            'Use <--> arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize +
-            borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
+        this.rocketPatrolMenu.displayWidth = game.config.width*1;
+        this.rocketPatrolMenu.scaleY = this.rocketPatrolMenu.scaleX;
+
+        // let menuConfig = {
+        //     fontFamily: 'Courier',
+        //     fontSize: '28px',
+        //     backgroundColor: '#F3B141',
+        //     color: '#843605',
+        //     align: 'right',
+        //     padding: {
+        //         top: 5,
+        //         bottom: 5,
+        //     },
+        //     fixedWidth: 0
+        // }
+
+        // this.add.text(game.config.width/2, game.config.height/2 - borderUISize -
+        //     borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
+        // this.add.text(game.config.width/2, game.config.height/2, 
+        //     'Use <--> arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        // menuConfig.backgroundColor = '#00FF00';
+        // menuConfig.color = '#000';
+        // this.add.text(game.config.width/2, game.config.height/2 + borderUISize +
+        //     borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
     
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
